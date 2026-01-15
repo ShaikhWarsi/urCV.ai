1b6c63ae1803c13fb3510f50800d94363c63dd08
Neel Pandey <neelpandeyofficial@gmail.com>
2026-01-15 21:26:12 +0530

    minor update

--- src/pages/Templates.tsx
+++ src/pages/Templates.tsx
@@ -41,7 +41,7 @@ const Templates = () => {
           <div className="flex items-center justify-between">
             <Link to="/" className="flex items-center space-x-2">
               <div className="w-8 h-8 rounded-lg flex items-center justify-center">
-                <img src="./websitelogo.png"/>
+                <img alt="img" src="./websitelogo.png"/>
               </div>
               <span className="text-2xl font-bold text-gray-900 dark:text-white">
                 urCV.ai
